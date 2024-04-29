54c1d8fc82be0e580fc023e14d8ca17f025f9738
류정우 <evencoding@gmail.com>
2024-04-30 00:15:13 +0900

    feat: appwrite logic

---
+++ components/FormField.jsx
@@ -0,0 +1,44 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { icons, images } from "../constants";
+
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+
+      <View className="w-full h-16 border-2 px-4 border-black-200 rounded-2xl focus:border-secondary items-center bg-black-100 flex-row">
+        <TextInput
+          {...props}
+          className="flex-1 text-white font-psemibold text-base w-full"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor="#7b7b8b"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+        />
+
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword((prev) => !prev)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className="w-6 h-6"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default FormField;
